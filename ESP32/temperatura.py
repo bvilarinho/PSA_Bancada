b1d44c9f07ee252fa1f60312267ee722c3c183d2
cdds-Carlos <101269506+cdds-Carlos@users.noreply.github.com>
2022-04-23 15:29:04 +0100

    Atualização PowerPooint

--- ESP32/temperatura.py
+++ ESP32/temperatura.py
@@ -30,6 +30,3 @@ while True:
     v = xx*0.000805 /303
     T = a0 + a1 * v + a2 * (v**2) +a3 * (v**3) + a4 * (v**4) +cjc
     print ( 'aux: ', aux, 'Valor ADC :', xx, 'V in : ',v, ' - T em ºC: - ', T)
-
-
-    
