bb3d69f27163c465974552e10712ccc73205ef5f
ivanpaiva72 <101653308+ivanpaiva72@users.noreply.github.com>
2022-06-03 16:19:26 +0100

    ficheiro certo do main

--- ESP/LIGAÇÂO SQL MQTT/main.py
+++ ESP/LIGAÇÂO SQL MQTT/main.py
@@ -1,16 +1,132 @@
-# This is a sample Python script.
+# python3.6
+###### XAMPP tem que estar aberto para inicializar o SQL
+import random
+import time
+from time import sleep
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import pymysql.cursors
+from paho.mqtt import client as mqtt_client
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+broker = '192.168.0.122'     #IP utilizado
+port = 1883
+topic = "temperatura"
+###
+topic2 = "ruído"
+topic3 = "vibração"
+topic4 = "humidade"
+###
+# generate client ID with pub prefix randomly
+client_id = f'python-mqtt-{random.randint(0, 100)}'
+# username = 'emqx'
+# password = 'public'
 
 
-# Press the green button in the gutter to run the script.
+# Connect to the database
+connection = pymysql.connect(host='localhost',
+                             user='root',
+                             password='',
+                             database='psa_bancada',
+                             cursorclass=pymysql.cursors.DictCursor)
+
+
+
+def connect_mqtt() -> mqtt_client:
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    client = mqtt_client.Client(client_id)
+    #client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+_temp = -1
+_hum = -1
+_aux = 1
+_vib = -1
+
+
+
+def subscribe(client: mqtt_client):
+    def on_message(client, userdata, msg):
+        #print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+        #print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
+        global _temp
+        global _hum
+        global _vib
+        global _aux
+
+        if (msg.topic == topic):
+            _temp = msg.payload.decode('utf8')
+            print(f"Received `{_temp}` from `{msg.topic}` topic")
+
+        if (msg.topic == topic4):
+            _hum = msg.payload.decode()
+            print(f"Received `{_hum}` from `{msg.topic}` topic")
+
+        if (msg.topic == topic3):
+            _vib = msg.payload.decode()
+            print('vibração', _vib)
+
+
+
+
+
+                # Connect to the database
+        connection = pymysql.connect(host='localhost',
+                                     user='root',
+                                     password='',
+                                     database='psa_bancada',
+                                     cursorclass=pymysql.cursors.DictCursor)
+
+
+
+        # connection with data base
+        with connection:
+            with connection.cursor() as cursor:
+                #Create a new record
+
+                if (msg.topic == topic):
+                    sql = ("INSERT INTO `dados` (`temperatura`) VALUES  ({})".format(_temp))
+                    cursor.execute(sql)
+
+                if (msg.topic == topic4):
+                    sql2 = ("INSERT INTO `dados` (`humidade`) VALUES  ({})".format(_hum))
+                    cursor.execute(sql2)
+
+
+
+
+            # connection is not autocommit by default. So you must commit to save
+            # your changes.
+            connection.commit()
+
+
+
+    # Leitura temperatura
+    client.subscribe(topic)
+    client.on_message = on_message
+
+    time.sleep(1)
+    # Leitura humidade
+    client.subscribe(topic4)
+    client.on_message = on_message
+
+
+
+
+
+
+def run():
+    client = connect_mqtt()
+    subscribe(client)
+    client.loop_forever()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    run()
